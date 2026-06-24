f10ab2a5e269eb27751ed9ad3d522f17fe1cc7a5
altas-dev-io <258637815+atlas-dev-io@users.noreply.github.com>
2026-06-24 08:12:26 +0800

    page: define initial page identifiers

---
+++ include/page/page.h
@@ -0,0 +1,21 @@
+#ifndef LUMINAFRAME_PAGE_H
+#define LUMINAFRAME_PAGE_H
+
+/*
+ * Application page identifiers.
+ */
+typedef enum{
+    /* Main menu */
+    MAIN_MENU_PAGE      ,
+
+    /* File browser */
+    IMAGE_BROWSING_PAGE ,
+
+    /* Image viewer */
+    IMAGE_VIEWING_PAGE  ,
+
+    /* Slideshow */
+    AUTO_PLAY_PAGE      ,
+} PageId;
+
+#endif /* LUMINAFRAME_PAGE_H */
