5baeb76aa67fa348d1b2bd6cd4bb773f5cc9f893
altas-dev-io <258637815+atlas-dev-io@users.noreply.github.com>
2026-06-24 08:13:23 +0800

    app: define initial application state

---
+++ include/app/app.h
@@ -0,0 +1,25 @@
+#ifndef LUMINAFRAME_APP_H
+#define LUMINAFRAME_APP_H
+
+#include "page/page.h"
+
+/*
+ * Global application state.
+ *
+ * All pages communicate through this structure.
+ */
+typedef struct{
+    /* Current active page. */
+    PageId current_page;
+
+
+    /* TODO: add global state. */
+    /* TODO: selected file. */
+    /* TODO: current directory. */
+    /* TODO: current image path. */
+    /* TODO: user input event. */
+} AppState;
+
+
+
+#endif /* LUMINAFRAME_APP_H */
